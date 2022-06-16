1675e030125d85395314b790d922594144395d9f
Nuno Antunes <nunontns@gmail.com>
2022-06-16 16:51:20 +0000

    Implement the changes from the tutorial

---
+++ routes/greet/[name].tsx
@@ -0,0 +1,12 @@
+/** @jsx h */
+import { h, PageProps } from "$fresh/runtime.ts";
+
+export default function GreetPage(props: PageProps) {
+    const { name } = props.params;
+    return (
+        <main>
+            <p>Greetings to you, {name}!</p>
+            {JSON.stringify(props)}
+        </main>
+    );
+}
--- routes/index.tsx
+++ routes/index.tsx
@@ -7,8 +7,8 @@ export default function Home() {
     <div>
       <img src="/logo.svg" height="100px" alt="the fresh logo: a sliced lemon dripping with juice" />
       <p>
-        Welcome to `fresh`. Try update this message in the ./routes/index.tsx
-        file, and refresh.
+        Welcome to `fresh`. <s>Try update this message in the ./routes/index.tsx
+          file, and refresh.</s>
       </p>
       <Counter start={3} />
     </div>
